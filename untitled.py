0c49ffed71854fa84073bd3ba6a35a63760f7317
Tisernik <99658293+Tisernik@users.noreply.github.com>
2024-12-23 08:59:31 +0300

    2024-12-23

--- untitled.py
+++
@@ -1,8 +0,0 @@
-from random import seed
-import requests
-
-url = "https://freewebnovel.com/lord-of-the-mysteries.html"
-session_obj = requests.Session()
-response = session_obj.get(url, headers={"User-Agent": "Mozilla/5.0"})
-
-print(response.content)
